6cf07542c350f7c652ac7fce49e10739a57dc853
Ininit <meininit@gmail.com>
2023-06-03 16:19:04 +0800

    feat: 移除 nvm, nrm 配置

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,29 +1,5 @@
 #!/usr/bin/env bash
 
-echo -e "\nInstalling nvm..."
-echo "=============================="
-
-if [ -e $HOME/.nvm ]; then
-  echo "nvm already installed... skipping."
-else
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-
-  nvm install --lts
-  nvm use stable
-fi
-
-echo -e "\nInstalling nrm..."
-echo "=============================="
-
-if nrm -V >/dev/null 2>&1; then
-  echo "nrm already installed... skipping."
-else
-  npm install nrm -g
-
-  nrm ls
-  # nrm use taobao
-fi
-
 echo -e "\nInstalling pnpm packages..."
 echo "=============================="
 
